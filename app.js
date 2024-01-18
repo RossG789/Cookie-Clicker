753072ddec33cd3c7b70ec5c4f1a85a27a1cabc6
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-18 16:00:59 +0000

    fourth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// let cookies = 0;
-// setInterval(function () {
-//   cookies += 1;
-//   document.getElementById("counted-cookies").innerHTML = cookies;
-// }, 1000);
+let cookies = 0;
+setInterval(function () {
+  cookies += 1;
+  document.getElementById("counted-cookies").innerHTML = cookies;
+}, 1000);
 
 console.log();
