d6757506b677e31933b6eeac2bd437ede967b88f
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-18 15:41:38 +0000

    third commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Yes");
+// let cookies = 0;
+// setInterval(function () {
+//   cookies += 1;
+//   document.getElementById("counted-cookies").innerHTML = cookies;
+// }, 1000);
+
+console.log();
