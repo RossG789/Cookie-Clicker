fa8e6649287249e37f0313792bacb6cf2f8c9fa8
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-19 14:02:05 +0000

    seventh

--- app.js
+++ app.js
@@ -4,25 +4,35 @@
 
 // Second Interval and Insertion function
 let cookies = 0;
+
 setInterval(function () {
   cookies += 1;
   document.getElementById("counted-cookies").innerHTML = cookies;
+  localStorage.setItem("localCookies", cookies);
+  console.log("cookies stored");
 }, 1000);
-
-// Adding click events to the second interval
 
 let cookieClick = document.getElementById("cookie");
 
 cookieClick.addEventListener("click", handleCookies);
 
-// Checking to see if cookiesCliked = to multiplier cost
-
 function handleCookies(event) {
+  // Animation stuff
   cookieClick.classList.remove("cookieanim"); // reset animation
   void cookieClick.offsetWidth; // trigger reflow
   cookieClick.classList.add("cookieanim"); // start animation
+  // End of animation stuff
   cookies += 1;
+  document.getElementById("counted-cookies").innerHTML = cookies;
   event.preventDefault();
   localStorage.setItem("localCookies", cookies);
-  console.log("cookies stored");
 }
+
+function getCookies() {
+  const localCookies = localStorage.getItem("localCookies");
+  document.getElementById("counted-cookies").innerHTML = localCookies;
+
+  console.log(localCookies);
+}
+
+getCookies(cookies);
